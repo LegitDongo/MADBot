c6dcf77b6c90a17afc08c8a5364e3e13cd731bc4
Legit Dongo <LegitDongo@gmail.com>
2018-10-02 22:39:33 -0500

    Telegram support!

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # Map-A-Droid User Submissions
 
-If you need user submissions through Discord and want it to work with Map-A-Droid, use this!
+If you need user submissions through Discord and/or Telegram and want it to work with Map-A-Droid, use this!
 
 ## How to use
 1. Install dependencies -- `npm install`
 2. Copy `config.ini.example` to `config.ini`
 3. Fill in the config settings appropriately
-    * `channel` - the specific channel id that will allow for user submissions
+    * `discordChannel` - the specific channel id that will allow for user submissions
         * Can also use `all` for all channels to be valid
+    * `telegramChat` - the specific chat id that will allow for user submissions
+        * Can also use `all` for all chats that the bot is in to register
     * `discordtoken` - the token given to you by discord when you set your bot up
         * Look here for further information [Creating a Discord Bot & Getting a Token](
         https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token)
+    * `telegramtoken` - the token given to you by the BotFather when you set your bot up
+        * See below for further instruction
     * `screenshotsLocation` - the path on your machine to the screenshots folder
 4. Run the bot! -  `node start.js`
     * It is highly suggested using a process manager for this such as [pm2](http://pm2.keymetrics.io/)
+    
+    
+### Telegram Broad Setup Guide
+1. Talk to the [BotFather](https://telegram.me/botfather) in the web client
+    * Begin the conversation with `/start`
+    * Proceed with `/newbot`
+    * It will ask you a few questions like what you want to name it and what username it should have and 
+    then give you a token - copy that into `telegramtoken` in your config
+2. Still talking to the BotFather, use `/setprivacy` and set it to `Disable` 
+-- **This has be done before step 3, otherwise you have to kick and re-add your bot**
+3. Add your bot to your group or channel - mobile app is best for doing this - 
+[example](https://stackoverflow.com/questions/33126743/how-do-i-add-my-bot-to-a-channel/33497769)
+4. Determine if you want to allow all places that the bot is included at to submit raid images or only one
+    * This is the `telegramChat` config option mentioned earlier
+    * To get a chat id, follow 
+    (this guide)[https://docs.influxdata.com/kapacitor/v1.5/event_handlers/telegram/#get-your-telegram-chat-id] 
--- start.js
+++ start.js
@@ -1,71 +1,146 @@
 var     fs          = require('fs'),
         request     = require('request'),
-        config      = JSON.parse(fs.readFileSync('./config.ini', 'utf8'));
-const   Discord     = require('discord.js'),
-        client      = new Discord.Client();
+        config      = JSON.parse(fs.readFileSync('./config.ini', 'utf8')),
+        Discord = false,
+        Telegram = false
+;
 
 var appexit = (err = null) => {
     if (err){
         console.log("\x1b[31m%s\x1b[0m", err);
     }
-    if (client){
-        client.destroy((err) => {
+    if (Discord){
+        Discord.destroy((err) => {
             console.log(err);
-    });
+        });
     }
     process.exit();
 };
 
+// set up services to send to
+if (typeof config.discordtoken !== 'undefined' && config.discordtoken !== ''){
+    if (typeof config.discordChannel === 'undefined' || config.discordChannel === ''){
+        appexit('You have to have the "discordChannel" section filled out in your config!');
+    }
+    // Login done in Discord block later
+    Discord = new (require('discord.js')).Client();
+
+}
+
+if (typeof config.telegramtoken !== 'undefined' && config.telegramtoken !== ''){
+    if (typeof config.telegramChat === 'undefined' || config.telegramChat === ''){
+        appexit('You have to have the "telegramChat" section filled out in your config!');
+    }
+    // Login done here for Telegram
+    Telegram = new (require('node-telegram-bot-api'))(config.telegramtoken, { polling: true });
+}
+
+if (!Discord && !Telegram){
+    appexit('You have to have at least one service (Discord or Telegram) set up!');
+}
+
 // Check required config options
-let required = ['channel', 'discordtoken', 'screenshotsLocation'];
+let required = ['screenshotsLocation'];
 for(let i in required){
     if (typeof config[required[i]] === 'undefined' || config[required[i]] === ''){
-        appexit('Config not filled out! You have to have the "channel", "discordtoken", and ' +
+        appexit('Config not filled out! You have to have the "channel", and ' +
             '"screenshotsLocation" sections filled out!');
     }
 }
 
-var clientready = () => {
-    console.log('Ready to go MAD');
+// Set up download function
+var download = (url, finishedCallback = false) => {
+    console.log('Downloading file: '+url);
+    request.get(url)
+        .pipe(fs.createWriteStream(config.screenshotsLocation + '/' +
+            'raidscreen_'+Date.now()+'_9999_9999_99.jpg'))
+        .on('close', function(){
+            console.log('Finished adding file');
+            if (finishedCallback){
+                finishedCallback();
+            }
+        })
+        .on('error', function (err) {
+            console.log(err);
+        });
 };
 
-var clientmessage = message => {
-    //don't respond if it's a bot
-    if (message.author.bot) return;
-    //don't respond if not on a text chat
-    if (message.channel.type !== 'text') return;
-    if (config.channel !== 'all' && message.channel.id !== config.channel) return;
-    if (message.attachments.size > 0){
-        message.attachments.forEach(function(attachment){
-            console.log('Downloading file: '+attachment.url);
-            request.get(attachment.url)
-                .pipe(fs.createWriteStream(config.screenshotsLocation + '/' +
-                    'raidscreen_'+Date.now()+'_9999_9999_99.jpg'))
-                .on('close', function(){
-                    console.log('Finished adding file');
+// Set up Discord requirements
+if (Discord) {
+    var discordReady = () => {
+        console.log('Ready to go MAD');
+    };
+
+    var discordMessage = message => {
+        //don't respond if it's a bot
+        if (message.author.bot) return;
+        //don't respond if not on a text chat
+        if (message.channel.type !== 'text') return;
+        if (config.discordChannel !== 'all' && message.channel.id !== config.discordChannel) return;
+        if (message.attachments.size > 0){
+            message.attachments.forEach(function(attachment){
+                download(attachment.url, () => {
                     message.channel.send('File successfully uploaded').then(m => {
                         m.delete(5000);
                     });
-                })
-                .on('error', function (err ) {
-                    console.log(err);
                 });
+            });
+        }
+    };
+
+    Discord.on('ready', discordReady);
+    Discord.on('message', discordMessage);
+    Discord.login(config.discordtoken);
+    Discord.on('disconnect', (event) => {
+        console.log(`Disconnected with code ${ event.code }`);
+        if(event.code !== 1006)
+            appexit();
+        else
+            Discord.destroy().then(() => Discord.login(config.discordtoken));
+    });
+}
+
+// Set up Telegram requirements
+if (Telegram){
+    var telegramErrorMessage = (errorObj) => {
+        // error.response is not available if code is EFATAL
+        if (errorObj.code === 'EFATAL'){
+            return 'Fatal error';
+        }
+        // error.response.body is a string if code is EPARSE
+        if (errorObj.code === 'EPARSE'){
+             return errorObj.response.body;
+        }
+        // error.response.body is an object if code is ETELEGRAM
+        if (errorObj.code === 'ETELEGRAM'){
+            return `(Code ${ errorObj.response.body.error_code }) ${ errorObj.response.body.description }`;
+        }
+    };
+
+    var telegramError = (type, error) => {
+        console.log(`${ type } error - ${ error.code }: ${ telegramErrorMessage(error) }`);
+    };
+
+    Telegram.on('photo', (msg) => {
+        if (config.telegramChat !== 'all' && msg.chat.id !== config.telegramChat) return;
+        let biggestPhoto = false;
+        let biggestPhotoSize = false;
+        for(let i = msg.photo.length - 1; i >= 0; i--){
+            if (msg.photo[i].file_size > biggestPhotoSize){
+                biggestPhoto = msg.photo[i];
+                biggestPhotoSize = msg.photo[i].file_size;
+            }
+        }
+        let link = Telegram.getFileLink(biggestPhoto.file_id).then((url) => {
+            download(url, () => {
+                Telegram.sendMessage(msg.chat.id, 'Photo successfully uploaded!');
+            });
         });
-    }
-};
+    });
+    Telegram.on('polling_error', (error) => { telegramError('Polling', error); });
+    Telegram.on('webhook_error', (error) => { telegramError('Webhook', error); });
 
-client.on('ready', clientready);
-client.on('message',  clientmessage);
-
-client.login(config.discordtoken);
-
-client.on('disconnect', (event) => {
-    console.log(`Disconnected with code ${ event.code }`);
-    if(event.code != 1006)
-        appexit();
-    else
-        client.destroy().then(() => client.login(config.discordtoken));
-});
+}
 
 //do some cleanup on ctrl + c
 process.on('SIGINT', () => {
